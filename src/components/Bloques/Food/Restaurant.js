e2070f006df308371947560161a41c2a19435306
aurelia-mp <aurelia.porolli@gmail.com>
2024-07-19 14:01:49 -0300

    actualizacion horario restaurante

--- src/components/Bloques/Food/Restaurant.js
+++ src/components/Bloques/Food/Restaurant.js
@@ -23,8 +23,10 @@ const Restaurant = () => {
         <p className='parrafo'>Encontrará el menú y propuesta en su Instragram:</p>
         <p className='parrafo'><a href="https://instagram.com/charquirestaurantok?igshid=NTc4MTIwNjQ2YQ==" rel="noreferrer" target="_blank">@charquirestaurantok</a></p>
         <p className='parrafo'>Horarios de apertura:</p>
-        <p className='parrafo'>Abierto para el brunch, almuerzo y cena de miércoles a sábado.
-Abierto solamente para el brunch y almuerzo los martes y domingos</p>
+        <p className='parrafo'>Martes: 12-15:30hs</p>
+        <p className='parrafo'>De miércoles a viernes: 12-15:30hs / 18hs a medianoche</p>
+        <p className='parrafo'>Sábado: de mediodía a medianoche</p>
+        <p className='parrafo'>Domingo 12-19hs</p>
         <p className='parrafo'>Cerrado los lunes</p>
     </div>
 
